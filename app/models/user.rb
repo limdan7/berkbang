c5175b35f9ff37706e129e056f038c92b39ab87c
limdan7 <limdan7@berkeley.edu>
2017-02-23 05:28:51 +0000

    profile photo

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   has_many :rooms, dependent: :destroy
   has_many :requests
   has_many :demands, dependent: :destroy
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   
   before_save{ self.email = email.downcase}
   # Include default devise modules. Others available are:
