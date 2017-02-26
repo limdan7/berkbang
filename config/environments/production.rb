86d49095d8a066273c917f16ad5fac49786c0709
limdan7 <limdan7@berkeley.edu>
2017-02-26 20:11:13 +0000

    production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.serve_static_files = true
   
   #email confirmation with SENDGRID
   config.action_mailer.delivery_method = :smtp
