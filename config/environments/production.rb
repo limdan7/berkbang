2a0bf57563e837c6a903eb22ce0c7388dbf9b0ad
limdan7 <limdan7@berkeley.edu>
2017-02-27 02:06:12 +0000

    variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
 config.paperclip_defaults = {
     storage: :s3,
     s3_credentials: {
-      bucket: berkbangimage,
+      bucket: 'berkbangimage',
       access_key_id: ENV["AWS_ACCESS_KEY_ID"],
       secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
       s3_region:us-west-1
