227b7dceba5db8bddde0bb4486d3b5ca1c75dfe2
limdan7 <limdan7@berkeley.edu>
2017-02-27 02:07:32 +0000

    string

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,7 +14,7 @@ config.paperclip_defaults = {
       bucket: 'berkbangimage',
       access_key_id: ENV["AWS_ACCESS_KEY_ID"],
       secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-      s3_region:us-west-1
+      s3_region: 'us-west-1'
       }
     } 
   # Code is not reloaded between requests.
